ca7a9345fe41fef73ad2caf8bef7bd0d2356fe68
jy03312 <jy03312@gmail.com>
2026-02-07 10:38:16 +0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/ui/globals.css";
+import PersistentDrawerLeft from "@/app/ui/home/drawerMenu";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppRouterCacheProvider>
+            <PersistentDrawerLeft>{children}</PersistentDrawerLeft>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
